251ad5cb575f2287678b0cf1f928eabd2b26ecca
Evgeny Kokatau <ekokotov@gmail.com>
2019-06-07 23:00:48 +0300

    add testing of server controlled pagination

--- __tests__/simple-local.tsx
+++ __tests__/simple-local.tsx
@@ -1,18 +1,17 @@
-import {mount, render} from 'enzyme';
-import waitUntil from "async-wait-until";
+import 'jsdom-global/register';
+import {mount} from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import waitUntil from 'async-wait-until';
 // import nock from 'nock';
 import React from 'react';
 import UrlTable from "../src";
 import {data} from './mock.json';
-const Enzyme = require('enzyme');
-const Adapter = require('enzyme-adapter-react-16');
-
 // automatically unmount and cleanup DOM after the test is finished.
-Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({adapter: new Adapter()});
 
 describe('Simple example with local data and paging', () => {
-
-    it('Component should render with header', (done) => {
+    it('Component should render with header', () => {
         const table = mount(
             <UrlTable
                 data={data}
@@ -25,10 +24,9 @@ describe('Simple example with local data and paging', () => {
 
         expect(renderedHeader.first().text()).toEqual('Name');
         expect(renderedHeader.last().text()).toEqual('Address');
-        done();
     });
 
-    it('should render local data without header', (done) => {
+    it('should render local data without header', () => {
         const table = mount(
             <UrlTable
                 data={data}
@@ -38,10 +36,9 @@ describe('Simple example with local data and paging', () => {
         );
 
         expect(table.find('thead th').exists()).toEqual(false);
-        done();
     });
 
-    it('should render local data with body', (done) => {
+    it('should render local data with body', () => {
         const table = mount(
             <UrlTable
                 data={data}
@@ -52,10 +49,9 @@ describe('Simple example with local data and paging', () => {
         );
 
         expect(table.find('tbody td').first().text()).toEqual(data[0].name);
-        done();
     });
 
-    it('should show working paging', async (done) => {
+    it('should show working paging', async () => {
         const PAGE_SIZE = 5;
         const calculatedPageCount = Math.round(data.length / PAGE_SIZE);
         const table = mount(
@@ -75,12 +71,11 @@ describe('Simple example with local data and paging', () => {
 
         expect(table.find('.table__pagination li.selected').text()).toEqual('1');
         table.find('.table__pagination li a').last().simulate('click');
-        await waitUntil(() => table.update());
+        await table.update();
         expect(table.find('.table__pagination li.selected').text()).toEqual('2');
-        done();
     });
 
-    it('should hide paging with pagination={false}', (done) => {
+    it('should hide paging with pagination={false}', () => {
         const table = mount(
             <UrlTable
                 data={data}
@@ -91,10 +86,9 @@ describe('Simple example with local data and paging', () => {
             />
         );
         expect(table.find('.table__pagination').exists()).toEqual(false);
-        done();
     });
 
-    it('should show only specified number of pages even if have more data (pageCount={N})', (done) => {
+    it('should show only specified number of pages even if have more data (pageCount={N})', () => {
         const PAGE_SIZE = 5;
         const PAGE_COUNT = 2;
         const table = mount(
@@ -110,7 +104,6 @@ describe('Simple example with local data and paging', () => {
             />
         );
         expect(table.find('.table__pagination li').length).toEqual(PAGE_COUNT + 2); // for prev and next
-        done();
     });
 
 });
